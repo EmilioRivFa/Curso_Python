afd7f0f37946ecef66a98b41ec9a372f68001d3a
EmilioRivFa <emiliorivfa@gmail.com>
2025-02-05 09:30:39 -0600

    import y variables

---
+++ Ejercicio2.py
@@ -0,0 +1,17 @@
+#Accedemos a otro archivo con el import en este caso a "constantes.py" en pocas palabras modulos externos
+import constantes
+#Como declaras variables:
+variable = True 
+print (variable)
+
+#Es importante destacar que este lenguaje de programación es muy exigente con la sangria.
+
+#Una constante se declara con mayusculas, una constante nunca va a cambiar.
+PI = 3.1416
+print(PI)
+print(constantes.datos)
+
+#tres variables que almacenan el mismo dato
+
+var1 = var2 = var3 = "Emilio", "Pablo", "Adrian"
+print(var1, var3, var2)
