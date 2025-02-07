fd3d5b9e9f02bb564e9fe274ac159ef30693215b
EmilioRivFa <emiliorivfa@gmail.com>
2025-02-07 10:07:02 -0600

    Tipos de datos

---
+++ Ejercicio3.py
@@ -0,0 +1,16 @@
+#Tipos de Datos
+#El dato input sirve para pedir un dato al usuariok
+cadena = input("Introduce una palabra: ")
+print(cadena)
+
+#Con el type sirve para saber el tipo de dato que se introduce
+print(type(cadena))
+
+#Con el int especificamos que queremos un valor de tipo entero
+numero = int(input("introduce un numero: "))
+print(numero)
+print(type(numero))
+#Con el float especificamos que queremos un valor de tipo float
+decimal = float(input("introduce un numero: "))
+print(decimal)
+print(type(decimal))
